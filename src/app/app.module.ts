ef384f3af06b686a0341fae8beab5752279e25f1
Blake Aghili <peopleofksmo@gmail.com>
2020-07-04 13:59:42 -0500

    Rest of the components. WILL ADD CSS and other beauty assets LATER.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductImageComponent } from './product-image/product-image.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsListComponent,
-    ProductRowComponent
+    ProductRowComponent,
+    ProductImageComponent,
+    PriceDisplayComponent,
+    ProductDepartmentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
