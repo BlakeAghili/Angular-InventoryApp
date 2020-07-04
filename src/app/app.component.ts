0495a331008dea2bfeadadc0fe365c65b5787d91
Blake Aghili <peopleofksmo@gmail.com>
2020-07-04 07:20:26 -0500

    One Product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import {Product} from './product.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'inventory-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'inventory-app';
+  product: Product;
+
+  constructor() {
+    let newProduct = new Product('NICEHAT'
+      , 'We Still Here Hat'
+      , '/resources/images/products/black-hat.jpg'
+      , ['Men', 'Accessories', 'Hats']
+      , 29.99);
+
+    this.product = newProduct;
+  }
 }
